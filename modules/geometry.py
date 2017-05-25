c182c17a11d99c4580cd5a7f0cc2209c8a33006a
Daniel Wyeth <daniel.wyeth@gmail.com>
2017-05-25 20:00:30 +0100

    Added a method in_bounds to the geometry module to check containment of a point.

--- modules/geometry.py
+++ modules/geometry.py
@@ -9,7 +9,7 @@ def voxel( *coordinates ) :
 
     if len( coordinates ) == 1 and isinstance( coordinates[ 0 ], N.ndarray ) :
         return coordinates[ 0 ].astype( N.int16 )
-    
+
     else :
         return N.array( coordinates, dtype=N.int16 )
 
@@ -23,5 +23,14 @@ def cuboid( minima, maxima ) :
     if isinstance( minima, N.ndarray ):
         return N.array( [ minima, maxima ] )
 
+
+def in_bounds( point, bounds ):
+
+    minimum, maximum = bounds
+    dimensions = range( 0, len( point ) )
+    for d in dimensions:
+        if ( point[ d ] < minimum[ d ] ) or ( maximum[ d ] < point[ d ] ):
+            return False
+    return True
 
 #---------------------------------------------------------------------------------------------------
