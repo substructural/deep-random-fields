d08ebc117efbc9bc6bd2408ebf944565f8710eb1
Daniel Wyeth <daniel.wyeth@gmail.com>
2017-07-26 02:45:19 +0100

    Modified the label conversion classes to work on single volumes.
    The functions in this module previously assumed that their input
    was structured by volume (and then patch) but this is no longer
    the case, now that the PatchSet returns a flat list.  As these
    functions make heavy use of permutations on the order of axes,
    this change was not so trivial as one might have expected, but it
    is done now, and the tests all pass (after much debugging ;-).

--- modules/labels.py
+++ modules/labels.py
@@ -106,18 +106,18 @@ import theano
 
 import pdb
 
+FLOAT_X = theano.config.floatX 
 
 #---------------------------------------------------------------------------------------------------
 
 
 def dense_patch_indices_to_dense_patch_distribution( indices, index_count ) :
 
-    volume_count = indices.shape[ 0 ]
-    patch_count = indices.shape[ 1 ]
-    patch_shape = indices.shape[ 2 : ]
+    patch_count = indices.shape[ 0 ]
+    patch_shape = indices.shape[ 1 : ]
 
-    distribution_shape = ( index_count, volume_count, patch_count, ) + patch_shape
-    distribution = numpy.zeros( distribution_shape ).astype( theano.config.floatX )
+    distribution_shape = ( index_count, patch_count ) + patch_shape
+    distribution = numpy.zeros( distribution_shape ).astype( FLOAT_X )
 
     for i in range( 0, index_count ) :
         distribution[ i ][ indices == i ] = 1.0
@@ -127,23 +127,24 @@ def dense_patch_indices_to_dense_patch_distribution( indices, index_count ) :
 
 
 
-def dense_patch_distribution_to_dense_volume_distribution( dense_patches, volume_shape ) :
+def dense_patch_distribution_to_dense_volume_distribution( dense_patches, target_shape ) :
 
-    volume_count = dense_patches.shape[ 0 ]
-    patch_count = dense_patches.shape[ 1 ]
-    patch_shape = dense_patches.shape[ 2 : -1 ]
+    patch_count = dense_patches.shape[ 0 ]
+    patch_shape = dense_patches.shape[ 1 : -1 ]
     index_count = dense_patches.shape[ -1 ]
+    assert len( patch_shape ) == 3
 
-    assert( len( patch_shape ) == 3 )
-    assert( len( volume_shape ) == 3 )
+    voxel_count = patch_count * reduce( operator.mul, patch_shape )
+    expected_voxel_count = reduce( operator.mul, target_shape )
+    assert voxel_count == expected_voxel_count
 
-    grid_dimensions = numpy.array( volume_shape ) / numpy.array( patch_shape )
+    grid_dimensions = numpy.array( target_shape ) // numpy.array( patch_shape )
     grid_shape = tuple( int( grid_dimensions[ i ] ) for i in range( 0, len( grid_dimensions ) ) )
-    grid_of_patches_shape = ( volume_count, ) + grid_shape + patch_shape + ( index_count, )
+    grid_of_patches_shape = grid_shape + patch_shape + ( index_count, )
     grid_of_patches = dense_patches.reshape( grid_of_patches_shape )
 
-    permutation = ( 0, 1, 4, 2, 5, 3, 6, 7 )
-    dense_volume_shape = ( volume_count, ) + volume_shape + ( index_count, )
+    permutation = ( 0, 3, 1, 4, 2, 5, 6 )
+    dense_volume_shape = target_shape + ( index_count, )
     dense_volume = numpy.transpose( grid_of_patches, permutation ).reshape( dense_volume_shape )
 
     return dense_volume
@@ -166,8 +167,7 @@ def dense_volume_indices_to_dense_volume_masks( dense_volume_indices, index_coun
     for i in range( 0, index_count ) :
         masked[ i ][ dense_volume_indices == i ] = 1
 
-    group_by_mask = ( 1, 0 ) + tuple( i + 1 for i in range( 1, volume_dimensions ) )
-    return numpy.transpose( masked, group_by_mask )
+    return masked
 
 
 
@@ -177,35 +177,31 @@ def dense_volume_indices_to_dense_volume_masks( dense_volume_indices, index_coun
 def dense_patch_distribution_to_sparse_patch_distribution( dense_patch_distribution ) :
 
     dimensions = dense_patch_distribution.shape
-    volume_count = dimensions[ 0 ]
-    patch_count = dimensions[ 1 ]
-    patch_shape = dimensions[ 2 : -1 ]
-    label_count = dimensions[ -1 ]
+    patch_count = dimensions[ 0 ]
+    patch_shape = dimensions[ 1 : -1 ]
 
     offset = tuple( int( d  / 2 ) for d in patch_shape )
-    sparse = numpy.array( [
-        [ [ dense_patch_distribution[ v, p ][ offset ]
-            for p in range( 0, patch_count ) ]
-        for v in range( 0, volume_count ) ] ] )
+    sparse = numpy.array( 
+        [ dense_patch_distribution[ p ][ offset ]
+          for p in range( 0, patch_count ) ])
 
     return sparse
 
 
 
-def sparse_patch_distribution_to_dense_volume_distribution( sparse_patch_distribution, volume_shape ) :
+def sparse_patch_distribution_to_dense_volume_distribution( sparse_patch_distribution, target_shape ) :
 
     dimensions = sparse_patch_distribution.shape
-    assert( len( dimensions ) == 3 )
-    assert( len( volume_shape ) == 3 )
+    assert( len( dimensions ) == 2 )
+    assert( len( target_shape ) == 3 )
 
-    volume_count = dimensions[ 0 ]
-    patch_count = dimensions[ 1 ]
-    label_count = dimensions[ 2 ]
-    voxel_count = reduce( operator.mul, volume_shape )
+    patch_count = dimensions[ 0 ]
+    label_count = dimensions[ 1 ]
+    voxel_count = reduce( operator.mul, target_shape )
     assert( patch_count == voxel_count )
 
-    k, j, i = volume_shape
-    dense_shape = ( volume_count, k, j, i, label_count, )
+    k, j, i = target_shape
+    dense_shape = ( k, j, i, label_count )
     return sparse_patch_distribution.reshape( dense_shape )
 
 
@@ -226,19 +222,13 @@ class DenseLabelConversions( object ):
             self.__label_count )
 
 
-    def labels_for_volumes( self, dense_patch_distribution, patch_grid_shape ):
+    def labels_for_volumes( self, dense_patch_distribution, target_shape ):
 
-        patch_count = dense_patch_distribution.shape[ 1 ]
-        assert patch_count == numpy.prod( patch_grid_shape )
-        assert len( dense_patch_distribution.shape ) == 6
-        assert len( patch_grid_shape ) == 3
-
-        patch_shape = dense_patch_distribution.shape[ 2:5 ]
-        volume_shape = patch_grid_shape * patch_shape
+        assert len( dense_patch_distribution.shape ) == 5
 
         dense_volume_distribution = dense_patch_distribution_to_dense_volume_distribution(
             dense_patch_distribution,
-            volume_shape )
+            target_shape )
 
         return dense_volume_distribution_to_dense_volume_indices( dense_volume_distribution )
 
--- modules/labels_tests.py
+++ modules/labels_tests.py
@@ -24,7 +24,7 @@ class TestData :
     @staticmethod
     def index_patches() :
 
-        return numpy.array( [ [
+        return numpy.array( [
 
             [ [ [  0,  1 ],
                 [  4,  5 ] ],
@@ -66,27 +66,27 @@ class TestData :
               [ [ 58, 59 ],
                 [ 62, 63 ] ] ]
 
-            ] ] )
+            ] )
 
 
     @staticmethod
     def disjoint_distribution_patches() :
 
         indices = TestData.index_patches()
-        distribution = numpy.zeros( ( 1, 8, 2, 2, 2, 64 ) )
+        distribution = numpy.zeros( ( 8, 2, 2, 2, 64 ) )
         for p in range( 0, 8 ) :
             for z in range( 0, 2 ) :
                 for y in range( 0, 2 ) :
                     for x in range( 0, 2 ) :
-                        d = indices[ 0, p, z, y, x ]
-                        distribution[ 0, p, z, y, x, d ] = 1.0
+                        d = indices[ p, z, y, x ]
+                        distribution[ p, z, y, x, d ] = 1.0
         return distribution
 
 
     @staticmethod
     def overlapping_distribution_patches() :
 
-        distribution = numpy.zeros( ( 1, 27, 2, 2, 2, 64 ) )
+        distribution = numpy.zeros( ( 27, 2, 2, 2, 64 ) )
         for k in range( 0, 3 ) :
             for j in range( 0, 3 ) :
                 for i in range( 0, 3 ) :
@@ -95,19 +95,19 @@ class TestData :
                             for x in range( 0, 2 ) :
                                 d = ( ( k + z ) * 16 ) + ( ( j + y ) * 4 ) + ( i + x )
                                 p = k * 9 + j * 3 + i
-                                distribution[ 0, p, z, y, x, d ] = 1.0
+                                distribution[ p, z, y, x, d ] = 1.0
         return distribution
 
 
     @staticmethod
     def distribution_volume() :
 
-        volume = numpy.zeros( ( 1, 4, 4, 4, 64 ) )
+        volume = numpy.zeros( ( 4, 4, 4, 64 ) )
         for z in range( 0, 4 ) :
             for y in range( 0, 4 ) :
                 for x in range( 0, 4 ) :
                     i = z*16 + y*4 + x
-                    volume[ 0, z, y, x, i ] = 1.0
+                    volume[ z, y, x, i ] = 1.0
         return volume
 
 
@@ -140,15 +140,15 @@ class DenseLabelTests( unittest.TestCase ) :
 
         distribution = TestData.distribution_volume()
         computed_indices = labels.dense_volume_distribution_to_dense_volume_indices( distribution )
-        expected_indices = TestData.index_volume().reshape( ( 1, 4, 4, 4 ) )
+        expected_indices = TestData.index_volume().reshape( ( 4, 4, 4 ) )
 
         self.assertTrue( numpy.array_equal( computed_indices, expected_indices ) )
 
 
     def test_dense_volume_indices_to_dense_volume_masks( self ) :
 
-        indices = TestData.index_volume().reshape( ( 1, 4, 4, 4 ) )
-        group_by_mask = ( 0, 4, 1, 2, 3 )
+        indices = TestData.index_volume().reshape( ( 4, 4, 4 ) )
+        group_by_mask = ( 3, 0, 1, 2 )
         distributions_as_masks = TestData.distribution_volume().astype( 'int16' )
         expected_masks = numpy.transpose( distributions_as_masks, group_by_mask )
         computed_masks = labels.dense_volume_indices_to_dense_volume_masks( indices, 64 )
@@ -162,29 +162,29 @@ class DenseLabelTests( unittest.TestCase ) :
 class SparseLabelTests( unittest.TestCase ) :
 
 
-
     def test_dense_patch_distribution_to_sparse_patch_distribution( self ) :
 
         dense_distribution = TestData.disjoint_distribution_patches()
-        expected_samples = dense_distribution[ :, :, 1, 1, 1, : ].reshape( 1, 1, 8, 64 )
+        expected_samples = dense_distribution[ :, 1, 1, 1, : ].reshape( 8, 64 )
         computed_samples = labels.dense_patch_distribution_to_sparse_patch_distribution( dense_distribution )
 
-        #pdb.set_trace()
         self.assertTrue( numpy.array_equal( computed_samples, expected_samples ) )
-
 
 
     def test_sparse_patch_distribution_to_dense_volume_distribution( self ) :
 
         dense_patches = TestData.overlapping_distribution_patches()
-        sparse_patches = dense_patches[ :, :, 1, 1, 1, : ].reshape( 1, 27, 64 )
-        computed_volume = labels.sparse_patch_distribution_to_dense_volume_distribution( sparse_patches, ( 3, 3, 3 ) )
-        expected_volume = TestData.distribution_volume()[ :, 1:, 1:, 1:, : ].reshape( ( 1, 3, 3, 3, 64 ) )
+        sparse_patches = dense_patches[ :, 1, 1, 1, : ].reshape( 27, 64 )
+        computed_volume = labels.sparse_patch_distribution_to_dense_volume_distribution(
+            sparse_patches,
+            ( 3, 3, 3 ) )
+        expected_volume = TestData.distribution_volume()[ 1:, 1:, 1:, : ].reshape( ( 3, 3, 3, 64 ) )
 
+        differences = computed_volume - expected_volume
+        print( 'computed:\n', computed_volume )
+        print( 'expected:\n', expected_volume )
+        print( 'different:\n', differences )
         self.assertTrue( numpy.array_equal( computed_volume, expected_volume ) )
-
-
-
 
 
 #---------------------------------------------------------------------------------------------------
