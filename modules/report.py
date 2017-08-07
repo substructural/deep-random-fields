e02cb132a1882b7c231ea1e66181906766041623
Daniel Wyeth <daniel.wyeth@gmail.com>
2017-08-07 15:48:13 +0100

    Added support for generating a html report of the output of an experiment.

---
+++ modules/report.py
@@ -0,0 +1,334 @@
+#===================================================================================================
+# report
+
+
+
+#---------------------------------------------------------------------------------------------------
+
+import os
+import os.path
+
+import labels
+from results import Images
+
+
+#---------------------------------------------------------------------------------------------------
+
+
+def html( experiment_name,
+          content,
+          page_foreground = '#000000',
+          page_background = '#ffffff',
+          image_table_foreground = '#ffffff',
+          image_table_background = '#000000' ):
+
+    return f'''
+
+<html>
+<style>
+
+    html
+    {{
+        width: 80%;
+        margin-left: auto;
+        margin-right: auto;
+    }}
+
+    body 
+    {{
+        color: {page_foreground};
+        background-color: {page_background};
+    }}
+
+    table.metrictable
+    {{
+        width: 80%;
+        margin: 50px auto;
+        text-align: left;
+    }}
+
+    img
+    {{
+        margin: 25px 25px;
+        text-align: center;
+        vertical-align: center;
+    }}
+
+    table.imagetable
+    {{
+        width: 80%;
+        margin: 50px auto;
+        color: {image_table_foreground};
+        background-color: {image_table_background};
+        text-align: center;
+    }}
+
+</style>
+
+<body>
+<h1>{experiment_name}</h1>
+
+{content}
+
+</body>
+</html>
+
+'''
+
+
+#---------------------------------------------------------------------------------------------------
+
+
+def metric_section(
+        metric_name,
+        statistics,
+        sample_images ): 
+
+    value_table = statistics_table( statistics )
+
+    mean_image_table    = image_table( 'mean '    + metric_name, sample_images.mean )
+    median_image_table  = image_table( 'median '  + metric_name, sample_images.median )
+    minimum_image_table = image_table( 'minimum ' + metric_name, sample_images.minimum )
+    maximum_image_table = image_table( 'maximum ' + metric_name, sample_images.maximum )
+
+    return f'''
+
+<hr>
+<h2>{metric_name}</h2>
+
+{value_table}
+
+{mean_image_table}
+{median_image_table}
+{minimum_image_table}
+{maximum_image_table}
+
+'''
+
+
+#---------------------------------------------------------------------------------------------------
+
+
+def statistics_table( statistics ):
+
+    return f'''
+
+<h3>overview</h3>
+<table class="metrictable">
+
+    <tr> <th> metric  </th> <th> value     </th> </tr>
+
+    <tr> <td> mean    </td> <td> {statistics.mean[0]}    </td> </tr>
+    <tr> <td> median  </td> <td> {statistics.median[0]}  </td> </tr>
+    <tr> <td> minimum </td> <td> {statistics.minimum[0]} </td> </tr>
+    <tr> <td> maximum </td> <td> {statistics.maximum[0]} </td> </tr>
+
+</table>
+
+'''
+
+
+#---------------------------------------------------------------------------------------------------
+
+
+def image_table( table_name, sample_images ):
+
+    body = ( 
+        image_row( 'axial', sample_images[ 0 ] ) + 
+        image_row( 'coronal', sample_images[ 1 ] ) + 
+        image_row( 'sagittal', sample_images[ 2 ] ) )
+
+    return f'''
+
+<h3>{table_name}</h3>
+<table class="imagetable">
+
+{body}
+
+</table>
+
+'''
+
+
+def image_row( label_for_row, image_paths ):
+
+    image_cells = ''.join( [ image_cell( p ) for p in image_paths ] )
+
+    return f'''
+
+    <tr>
+        <td>{label_for_row}</td>
+        {image_cells}    
+    </tr>
+
+'''
+
+
+def image_cell( image_path ):
+
+    return f'''
+        <td><image src="{image_path}"></td> '''
+
+
+#---------------------------------------------------------------------------------------------------
+
+
+class SourceData( object ):
+
+
+    @staticmethod
+    def representative_volumes_for_metrics( metrics, dataset ):
+
+        indices = set( index for metric in metrics for statistic, index in metric )
+        volumes = { i: dataset.validation_set[ i ].read_volume() for i in indices }
+
+        return volumes
+
+
+    @staticmethod
+    def representative_distributions_and_offsets_for_metrics( metrics, results ):
+
+        indices = set( index for metric in metrics for statistic, index in metric )
+        distribution_and_ofsets = {
+            i: results.predicted_distribution( i ) for i in indices }
+
+        distributions = { i : d for i, (d, o) in distribution_and_ofsets.items() }
+        offsets = { i : o for i, (d, o) in distribution_and_ofsets.items() }
+
+        return distributions, offsets
+
+
+    @staticmethod
+    def image_data_from_volumes( volumes, offsets, target_shape ):
+
+        return { i : Images.extract( volumes[ i ].image_data, offsets[ i ], target_shape )
+                 for i in volumes }
+
+
+    @staticmethod
+    def reference_labels_from_volumes( volumes, offsets, target_shape ):
+
+        return { i : Images.extract( volumes[ i ].label_data, offsets[ i ], target_shape )
+                 for i in volumes }
+
+
+    @staticmethod
+    def predicted_labels_from_distributions( distributions ):
+
+        return { i : labels.dense_volume_distribution_to_dense_volume_indices( d )
+                 for i, d in distributions.items() }
+    
+
+
+#---------------------------------------------------------------------------------------------------
+
+class Report( object ):
+
+
+    @staticmethod
+    def write( results, dataset, target_shape ):
+
+        class_count = results.class_count
+
+        dice = results.statistics_for_mean_dice_score_per_volume
+        dice_per_class = [
+            results.statistics_for_dice_score_for_class( i )
+            for i in range( class_count ) ]
+        
+        metrics = [ dice ] + dice_per_class
+
+        volumes = SourceData.representative_volumes_for_metrics( metrics, dataset )
+        distributions, offsets = SourceData.representative_distributions_and_offsets_for_metrics(
+            dice_per_class,
+            results )
+
+        image_data = SourceData.image_data_from_volumes( volumes, offsets, target_shape )
+        reference = SourceData.reference_labels_from_volumes( volumes, offsets, target_shape )
+        predicted = SourceData.predicted_labels_from_distributions( distributions )
+
+        archive = results.archive
+        overview = Report.section_for_overview( dice, image_data, predicted, reference, results )
+        section_per_class = [
+            Report.section_for_class(
+                c, dice_per_class[ c ], image_data, predicted, reference, results )
+            for c in range( class_count ) ]
+
+        report_name = results.results_id.replace( '_', ' ' )
+        file_content = html( report_name, overview + '\n'.join( section_per_class ) )
+        file_name = archive.saved_object_file_name( 'report' ) + '.html' 
+        with open( file_name, 'w' ) as output:
+            output.write( file_content )
+
+        return file_name
+
+
+    @staticmethod
+    def section_for_overview( statistics, image_data, predicted, reference, results ):
+         
+        name = f'dice over all classes'
+        method = Images.sample_difference_of_multiple_masks
+        return Report.section(
+            name, statistics, image_data, predicted, reference, method, results )
+
+    
+    @staticmethod
+    def section_for_class( c, statistics_for_c, image_data, predicted, reference, results ):
+         
+        name = f'dice for class {c}'
+        method = lambda i, p, r, n : Images.sample_difference_of_masks( i, p, r, n, c )
+        return Report.section(
+            name, statistics_for_c, image_data, predicted, reference, method, results )
+
+    
+    @staticmethod
+    def section( name, statistics, image_data, predicted, reference, sample_method, results ):
+        
+        names = statistics._fields
+
+        images_per_statistic = [
+            sample_method(
+                image_data[ volume_index ],
+                predicted[ volume_index ],
+                reference[ volume_index ],
+                results.class_count )
+            for value, volume_index in statistics ]
+
+        image_file_names_per_statistic = [
+            Report.save_sample_images_for_statistic(
+                images,
+                f'{name}-{names[ statistic ]}',
+                results.archive )
+            for statistic, images in enumerate( images_per_statistic ) ]
+
+        samples_indexed_by_statistic_name = Images.Samples( **{
+            names[ statistic ] : image_file_names
+            for statistic, image_file_names in enumerate( image_file_names_per_statistic ) } )
+        
+        return metric_section( name, statistics, samples_indexed_by_statistic_name )
+        
+
+    @staticmethod
+    def save_sample_images_for_statistic( sample_images_for_statistic, section_name, archive ):
+
+        axis_count = len( Images.Axes )
+        position_count = len( Images.SamplePositions )
+        assert sample_images_for_statistic.shape[ 0 : 2 ] == ( axis_count, position_count )
+    
+        prefix = 'report-' + section_name.replace( ' ', '_' )
+        file_names = (
+            [ [ archive.saved_object_file_name(
+                    prefix,
+                    f'{axis.name}-{position.name}.png' )
+                for position in Images.SamplePositions ]
+              for axis in Images.Axes ] )
+
+        for i in range( axis_count ):
+            for j in range( position_count ):
+                if os.path.exists( file_names[ i ][ j ] ):
+                    os.remove( file_names[ i ][ j ] )
+                Images.save_image( sample_images_for_statistic[ i ][ j ], file_names[ i ][ j ] )
+
+        return file_names
+
+
+#---------------------------------------------------------------------------------------------------
---
+++ modules/report_tests.py
@@ -0,0 +1,185 @@
+#===================================================================================================
+# report tests
+
+
+
+#---------------------------------------------------------------------------------------------------
+
+import os
+import unittest
+
+import numpy
+
+import geometry
+
+from results import SegmentationResults
+from report import Report
+
+
+#---------------------------------------------------------------------------------------------------
+
+
+def input_directory():
+
+    path = os.path.abspath( os.path.dirname( __file__ ) ) + '/test/input'
+    if not os.path.exists( path ):
+        raise Exception( f'input directory "{path}" does not exist' )
+
+    return path
+
+
+def output_directory():
+
+    path = os.path.abspath( os.path.dirname( __file__ ) ) + '/test/output'
+    if not os.path.exists( path ):
+        os.mkdir( path )
+
+    return path
+
+
+
+#---------------------------------------------------------------------------------------------------
+
+
+class MockVolume( object ):
+
+
+    @staticmethod
+    def define( bounds, offset, outer_span ):
+
+        neg = numpy.logical_not
+
+        base_image = numpy.array(
+            [ [ [ z + y + x
+                  for x in range( bounds[2] ) ]
+                for y in range( bounds[1] ) ]
+              for z in range( bounds[0] ) ] )
+
+        border = numpy.array(( 10, 10, 10 ))
+        mask1 = geometry.mask( bounds, offset + border, offset + outer_span - border )
+        mask2 = geometry.mask( bounds, offset, offset + outer_span ) & neg( mask1 )
+        image = base_image + ( mask1 * 40 ) + ( mask2 * 40 )
+
+        labels = numpy.zeros(( bounds )) + ( mask1 * 1 ) + ( mask2 * 2 )
+
+        return image, labels
+
+
+    def __init__( self, bounds, offset, outer_span ):
+
+        image, labels = MockVolume.define( bounds, offset, outer_span )
+        self.image_data = image
+        self.label_data = labels
+
+
+class MockAquisition( object ):
+
+    def __init__( self, volume ):
+
+        self.volume = volume
+
+
+    def read_volume( self ):
+
+        return self.volume
+
+
+class MockDataset( object ):
+
+    def __init__( self, bounds, offsets, reference_spans ):
+
+        assert len( offsets.shape ) == 2
+        assert offsets.shape == reference_spans.shape
+
+        count = offsets.shape[0]
+        self.validation_set = [
+            MockAquisition( 
+                MockVolume( bounds, offsets[ i ], reference_spans[ i ] ) )
+            for i in range( count ) ]
+
+
+class MockResults( SegmentationResults ):
+
+    
+    @staticmethod
+    def distribution( target, span, value ):
+
+        neg = numpy.logical_not
+
+        border = numpy.array(( 10, 10, 10 )).astype( 'int64' )
+        zeros = numpy.zeros(( 3, )).astype( 'int64' )
+        mask1 = geometry.mask( target, border, span - border )
+        mask2 = geometry.mask( target, zeros, span ) & neg( mask1 )
+        mask0 = neg( mask1 | mask2 )
+
+        distribution0 = mask0 * value
+        distribution1 = mask1 * value
+        distribution2 = mask2 * value
+
+        distribution = numpy.array([ distribution0, distribution1, distribution2 ])
+        return numpy.transpose( distribution, ( 1, 2, 3, 0 ) )
+
+
+    def __init__( self, target, offsets, predicted_spans, reference_spans, data_path, name ):
+
+        assert len( offsets.shape ) == 2
+        assert offsets.shape == predicted_spans.shape
+
+        class_count = 3
+
+        super( MockResults, self ).__init__( data_path, name, 0, class_count )
+
+        self.offsets = offsets
+        self.volume_count = offsets.shape[0]
+
+        self.predicted = [
+            MockResults.distribution( target, predicted_spans[ i ], 0.5 )
+            for i in range( self.volume_count ) ]
+
+        self.reference = [
+            MockResults.distribution( target, reference_spans[ i ], 1.0 )
+            for i in range( self.volume_count ) ]
+
+        for i in range( self.volume_count ):
+            self.append( self.predicted[ i ], self.reference[ i ] )
+        
+
+    def predicted_distribution( self, i ):
+
+        return ( self.predicted[ i ], self.offsets[ i ] )
+
+
+
+#---------------------------------------------------------------------------------------------------
+
+
+def write_report():
+
+    unit = numpy.ones(( 3, )).astype( 'int64' )
+    bounds = 200 * unit
+    target = 150 * unit
+
+    offsets = numpy.array([ ( i * 10 + 10 ) * unit for i in range( 5 ) ])
+    outer_spans = numpy.array([ ( i * 20 + 60 ) * unit for i in range( 5 ) ])  
+    inner_spans = numpy.array([ ( i * 10 + 60 ) * unit for i in range( 5 ) ])
+
+    dataset = MockDataset( bounds, offsets, outer_spans )
+    results = MockResults(
+        target, offsets, inner_spans, outer_spans, output_directory(), 'report_test' )
+
+    Report.write( results, dataset, target )
+
+
+
+class ReportTests( unittest.TestCase ):
+
+
+    def test_by_regression( self ):
+
+        write_report()
+
+         
+
+
+
+#---------------------------------------------------------------------------------------------------
