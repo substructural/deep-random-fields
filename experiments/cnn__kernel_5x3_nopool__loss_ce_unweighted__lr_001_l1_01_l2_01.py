7aa40eae95f7c1375234d2ddfb69e980f4462cc8
Daniel Wyeth <daniel.wyeth@gmail.com>
2017-08-09 02:50:30 +0100

    Added the initial set of four basic CNN experiments.
    These experiments evaluate the effect of kernel size (either 3^3 or 5^3)
    and weighting on the cross entropy cost (all four combinations).  They
    run for only a single epoch, but with the full 250 volume training set
    (1140 batches of 1000 patches, with 10^3 labels per patch, or roughly
    1.14 billion training samples).  This should be enough to get a sense
    of their performance.
    
    Future experiments on these architectures should examine the learning
    rate and the regularisation parameters.  As the batch only uses about
    4GB on the GPU it might also be worth upping the batch size (for smaller
    networks such as these) or upping the network size (if these experiments
    do not indicate overfitting).

---
+++ experiments/cnn__kernel_5x3_nopool__loss_ce_unweighted__lr_001_l1_01_l2_01.py
@@ -0,0 +1,92 @@
+#===================================================================================================
+# experiment tests
+
+
+
+#---------------------------------------------------------------------------------------------------
+
+import os
+import sys
+import unittest
+
+import activation
+import costs
+import data
+import experiments
+import layers
+import learning_rates
+import network
+import oasis
+import optimisation
+import output
+import sample
+import report
+
+
+#---------------------------------------------------------------------------------------------------
+
+
+class Definition( experiments.ExperimentDefinition ):
+
+
+    @property
+    def experiment_id( self ):
+
+        return 'cnn__kernel_5x3_nopool__loss_ce_unweighted__lr_001_l1_01_l2_01'
+
+
+    @property
+    def label_count( self ):
+
+        return 4
+
+
+    @staticmethod
+    def dataset( input_path, log ):
+
+        return oasis.OasisDataSet( input_path, 100, 50, 250, 42, maybe_log = log )
+
+
+    @property
+    def sample_parameters( self ):
+
+        return ( sample.Parameters()
+                 .with_volume_count( 10 )
+                 .with_window_margin( 6 )
+                 .with_target_shape(( 170, 200, 160 ))
+                 .with_patch_shape(( 22, 22, 22 ))
+                 .with_patch_count( 1000 )
+                 .with_patch_stride( 10 ) )
+
+
+    @staticmethod
+    def model():
+
+        leaky_relu = activation.LeakyRectifiedLinearUnit( 0.1 )
+        architecture = network.Architecture(
+            [ layers.ConvolutionalLayer(  1, 16, ( 5, 5, 5 ), 1, leaky_relu ),
+              layers.ConvolutionalLayer( 16, 32, ( 5, 5, 5 ), 1, leaky_relu ),
+              layers.ConvolutionalLayer( 32,  4, ( 5, 5, 5 ), 1, leaky_relu ),
+              layers.Softmax(),
+              layers.ScalarFeatureMapsProbabilityDistribution()
+            ],
+            input_dimensions = 4,
+            output_dimensions = 5 )
+
+        return network.Model( architecture, seed = 42 )
+
+
+    def optimiser( self, dataset, log ):
+
+        distribution_axis = 1
+        cost_function = costs.CategoricalCrossEntropyCost(
+            distribution_axis, weight_L1=0.01, weight_L2=0.01 )
+
+        learning_rate = learning_rates.RMSPropLearningRate( 1e-3, 0.9 )
+        parameters = optimisation.Parameters( maximum_epochs=1 )
+        return optimisation.StochasticGradientDescent(
+            parameters, cost_function, learning_rate, log )
+
+
+#---------------------------------------------------------------------------------------------------
+        
